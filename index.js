f81678ae923bcf2488d8cfff9a59c00417a014ea
Anthony <anthonykimlao@gmail.com>
2023-10-08 15:01:23 -0500

    modifying

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent = "Anthony is the champion"
+document.body.append(newHeader);
+
+
+
+/*const terms = document.createElement(‘p’);
+terms.setAttribute(‘id’,‘para-1’);
+fieldNameElement.textContent = "New text";
+newHeader.textContent = "Anthony is the champion"
+
+const newHeader = document.createElement("h1");
+newHeader.id = "newHeaderId";
+document.body.appendChild(newHeader);
+*/
